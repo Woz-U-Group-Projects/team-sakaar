c19bbbc436e6f355188739bb9d7f381564ae150d
Steve <steve_chinburg@hotmail.com>
2019-10-17 20:02:35 -0500

    steve

--- server-express-mysql/routes/tasks.js
+++ server-express-mysql/routes/tasks.js
@@ -1,10 +1,10 @@
-var express = require("express");
++var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
 router.get("/", function(req, res, next) {
   models.Task.findAll().then(tasks => res.json(tasks));
-});
+});.
 
 router.post("/", function(req, res, next) {
   let newTask = new models.Task();
