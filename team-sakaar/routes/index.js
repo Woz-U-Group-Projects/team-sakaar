51521e8ad3741b67185e110035534bd05e981eee
Steve <steve_chinburg@hotmail.com>
2019-10-22 11:00:10 -0500

    new code

--- team-sakaar/routes/index.js
+++ team-sakaar/routes/index.js
@@ -1,18 +1,52 @@
-// 1
-const http = require('http');
+var express = require('express');
+var router = express.Router();
 
-// 2
-const hostname = 'localhost';
-const port = 3000;
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Users',
+    name: [
+      {
+        name: '',
+        state: 'CA',
+        PostalCode: ,
+        email:
 
-// 3
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Welcome to the world of Node development!\n');
+
+        
+      },
+      {
+        name: 'Denver',
+        state: 'CO',
+        population: 682545
+      },
+      {
+        name: 'Seattle',
+        state: 'WA',
+        population: 704352
+      },
+      {
+        name: 'Portland',
+        state: 'OR',
+        population: 639863
+      },
+      {
+        name: 'New York City',
+        state: 'NY',
+        population: 8.538
+      },
+      {
+        name: 'Charlotte',
+        state: 'NC',
+        population: 842051
+      },
+      {
+        name: 'Pittsburgh',
+        state: 'PA',
+        population: 303625
+      }
+    ]
+  });
 });
 
-// 4
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-})
+module.exports = router;
