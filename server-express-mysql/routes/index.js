a638813896f297ffd75ec81a6742d700e077157f
Josh Engelbrecht <jshnglbrcht@gmail.com>
2019-10-23 23:18:24 -0400

    added sequelize DB migrations

--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -1,21 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Password1!',
-  database: 'SakaarDB'
-
-});
-
-connection.connect(function(err){
-  if(err){
-    console.log(err);
-  } else
-  console.log("We are connected to the database!");
-})
+const mysql = require('mysql2');
 
 
 /* GET home page. */
