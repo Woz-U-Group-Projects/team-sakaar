d972bd0c22f89c0b11fe65ccd862cd8ff64d3c9b
Michael Reynolds <michaelreynolds537@gmail.com>
2019-11-16 15:52:19 -0800

    StaticModels/genres imported to routes/index.js file
    Method added to allow data to be accessed by Front End application.

--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require('express'); 
 var router = express.Router();
 const mysql = require('mysql2');
 var models = require('../models');
-var staticModels = require('../staticModels/genres'); 
+var staticModels = require('../staticModels/genres'); //Add this line 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { myName: 'Team-Sakaar' });
 });
 
+ // Add this function below
 router.get('/staticGenres', function (req, res, next) {
 
   res.send(JSON.stringify(
