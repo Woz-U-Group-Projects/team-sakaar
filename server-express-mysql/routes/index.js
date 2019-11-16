9067d5df04afdff6806f360b8bfc26dd2b9f077d
Michael Reynolds <michaelreynolds537@gmail.com>
2019-11-15 17:55:15 -0800

    Static Model created.
    Genres for drop-down menu

--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const mysql = require('mysql2');
-var models = require('../models');  // <--- Add this line
+var models = require('../models');
+var staticModels = require('../staticModels/genres'); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { myName: 'Team-Sakaar' });
+});
+
+router.get('/staticGenres', function (req, res, next) {
+
+  res.send(JSON.stringify(
+    staticModels.genre
+  ));
 });
 
 module.exports = router;
