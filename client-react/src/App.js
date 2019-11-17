6dd9ddd5fa80e3de87da82fbd45c0e9c4705e820
Michael Reynolds <michaelreynolds537@gmail.com>
2019-11-16 18:10:43 -0800

    Genres imported to  App.js file.
    Access to Genres uri.

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Project from "./components/Project";
 import "./App.css";
+import Genres from './components/Genres';
 
 function App() {
   return (
     <div className="App">
       <Project />
+      <Genres uri="http://localhost:3001/staticGenres" />
     </div>
   );
 }
